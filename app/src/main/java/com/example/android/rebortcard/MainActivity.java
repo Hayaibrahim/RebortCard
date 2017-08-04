fe83a1a69e7116cec7cf76092b69b4f40a74cbb4
Hayaibrahim <Haya.aldkaan@gmail.com>
2017-08-04 11:18:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/rebortcard/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.rebortcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Rebortcard h = new Rebortcard("haya", 34567, "math", 80);
+        System.out.println(h);
+    }
+}
---
+++ app/src/main/java/com/example/android/rebortcard/Rebortcard.java
@@ -0,0 +1,57 @@
+package com.example.android.rebortcard;
+
+import java.util.List;
+
+/**
+ * Created by haya1 on 8/4/2017 AD.
+ */
+public class Rebortcard {
+    String name;
+    int id;
+    String Subject;
+    int grade;
+
+    public Rebortcard(String name, int id, String Subject, int grade) {
+        this.name = name;
+        this.id = id;
+        this.Subject = Subject;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSubject(String Subject) {
+        this.Subject = Subject;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentName=" + name + "\nidnumber=" + id + "\nSubject=" + Subject + "\ngrade=" + grade;
+    }
+}
